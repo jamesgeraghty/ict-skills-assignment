0bdd0274e7b2d2351f75329f60c1762fc05a776b
Glitch (zealous-sleet-helicona) <none>
2020-07-26 20:06:29 +0000

    ⏰📖 Checkpoint ./controllers/about.js:17958384/115 ./controllers/dashboard.js:17958384/123 ./views/dashboard.hbs:17958384/922 ./views/partials/listsongs.hbs:17958384/1739

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -5,9 +5,9 @@ const logger = require("../utils/logger");
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
-    const viewData = {
-      title: "Template 1 Dashboard",
-    };
+     const viewData = {
+      title: 'Playlist 1 Dashboard'
+    }
     response.render("dashboard", viewData);
   },
 };
